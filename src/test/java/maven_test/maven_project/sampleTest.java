67a37e146224e53a4724f17eecbe39c1ae505e91
Abhijnan <abhijnan.kundu@gmail.com>
2024-03-15 20:27:31 -0700

    modify first commit

--- src/test/java/maven_test/maven_project/sampleTest.java
+++ src/test/java/maven_test/maven_project/sampleTest.java
@@ -2,31 +2,23 @@ package maven_test.maven_project;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
+
+
 public class sampleTest {
 	
-	
-	
-	@Test
-	public void smoke() {
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
-		driver.manage().deleteAllCookies();
-		driver.get("https://www.google.com");
-		driver.manage().window().maximize();
-		driver.navigate().refresh();
+	@BeforeTest
+	public void launch_site() {
 		
-		
-		System.out.println("perform a smoke test after the feature change");
-
-
-		System.out.println("More Changes from Abhi-New branch");
-
-
 	}
+	
+	
+	
+
 
 
 
